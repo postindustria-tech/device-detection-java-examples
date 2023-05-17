d83e5eefc0c774c5b47ccdb2d32ec6b7c0613664
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-17 15:19:48 +0100

    Update run-unit-tests.ps1

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -6,11 +6,39 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName
 )
-$ExtraArgs = @'
--DTestResourceKey={0} -DSuperResourceKey={1} -DLicenseKey={2}
-'@ -f $Keys.TestResourceKey, $Keys.TestResourceKey, $Keys.DeviceDetection
 
-Write-Output $ExtraArgs
-./java/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -ExtraArgs $ExtraArgs
+$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+
+Write-Output "Entering '$RepoPath'"
+Push-Location $RepoPath
+
+try {
+    
+    Write-Output "Testing '$Name'"
+    mvn clean test -f pom.xml -DXmx2048m --no-transfer-progress -DfailIfNoTests=false "-Dhttps.protocols=TLSv1.2" "-DTestResourceKey=$($Keys.TestResourceKey)" "-DSuperResourceKey=$($Keys.TestResourceKey)" "-DLicenseKey=$($Keys.DeviceDetection)" 
+
+    # Copy the test results into the test-results folder
+    Get-ChildItem -Path . -Directory -Depth 1 | 
+    Where-Object { Test-Path "$($_.FullName)\pom.xml" } | 
+    ForEach-Object { 
+        $targetDir = "$($_.FullName)\target\surefire-reports"
+        $destDir = ".\test-results\unit"
+        if(!(Test-Path $destDir)) { New-Item -ItemType Directory -Path $destDir }
+        if(Test-Path $targetDir) {
+            Get-ChildItem -Path $targetDir | 
+            Where-Object { $_.Name -notlike "*ExampleTests*"} |
+            ForEach-Object {
+                Copy-Item -Path $_.FullName -Destination $destDir
+            }
+        }
+    }
+
+}
+finally {
+
+    Write-Output "Leaving '$RepoPath'"
+    Pop-Location
+
+}
 
 exit $LASTEXITCODE
