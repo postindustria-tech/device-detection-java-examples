bb91195b97dc9deadfca11a47fde7c0c91dc2f3e
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-17 14:48:56 +0100

    Update run-unit-tests.ps1

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -7,6 +7,8 @@ param(
     [string]$RepoName
 )
 $ExtraArgs = '"-DTestResourceKey=$($Keys.TestResourceKey)" "-DSuperResourceKey=$($Keys.TestResourceKey)" "-DLicenseKey=$($Keys.DeviceDetection)"'
+
+Write-Output $ExtraArgs
 ./java/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -ExtraArgs $ExtraArgs
 
 exit $LASTEXITCODE
